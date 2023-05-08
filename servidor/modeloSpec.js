4e68c542ca4ee20fc2d4a32291ae1eb656790e7e
ajtarraga <antoniojoaquin231999@gmail.com>
2023-05-08 16:58:06 +0200

    Add pruebas

---
+++ servidor/modeloSpec.js
@@ -0,0 +1,23 @@
+describe("El sistema CAU...", function () {
+    let modelo = require("./modelo.js");
+    var sistema;
+    var us1, us2, us3;
+
+    beforeEach(function () {
+        sistema = new modelo.Sistema();
+        sistema.agregarUsuario("pepe", "normal");
+        sistema.agregarUsuario("luis", "normal");
+        sistema.agregarUsuario("luisa", "tecnico");
+        us1 = sistema.obtenerUsuario("pepe");
+        us2 = sistema.obtenerUsuario("luis");
+        us3 = sistema.obtenerUsuario("luisa");
+    });
+
+    it("comprobamos los nick de los usuarios", function () {
+        expect(us1.nick).toEqual("pepe");
+        expect(us2.nick).toEqual("luis");
+        expect(us3.nick).toEqual("luisa");
+        expect(us3.esTecnico()).toEqual(true);
+    });
+
+});
